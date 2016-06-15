1da56db66554bc4260ba01534eda5df45cf9ce29
tujasiri <tujasiri@yahoo.com>
2016-06-15 16:32:10 -0500

    added google music objects

--- app/src/main/java/com/truthuniversal/tujasiri/truthuniversalapp/GoogleMusicAlbumItem.java
+++ app/src/main/java/com/truthuniversal/tujasiri/truthuniversalapp/GoogleMusicAlbumItem.java
@@ -4,4 +4,70 @@ package com.truthuniversal.tujasiri.truthuniversalapp;
  * Created by tujasiri on 6/14/2016.
  */
 public class GoogleMusicAlbumItem {
+
+        private String gma_album_id = "";
+        private String gma_album_title = "";
+        private String gma_album_image_url = "";
+        private String gma_album_artist_name = "";
+        private double gma_album_price = 0.0;
+
+
+        public GoogleMusicAlbumItem(){
+            String gma_album_id = "";
+            String gma_album_title = "";
+            String gma_album_image_url = "";
+            String gma_album_artist_name = "";
+            double gma_album_price = 0.0;
+
+
+            this.gma_album_id = gma_album_id;
+            this.gma_album_title = gma_album_title;
+            this.gma_album_image_url = gma_album_image_url;
+            this.gma_album_artist_name = gma_album_artist_name;
+            this.gma_album_price = gma_album_price;
+        }
+
+        public String getGma_album_id() {
+            return gma_album_id;
+        }
+
+        public String getGma_album_title() {
+            return gma_album_title;
+        }
+
+        public String getGma_album_image_url() {
+            return gma_album_image_url;
+        }
+
+        public String getGma_album_artist_name() {
+            return gma_album_artist_name;
+        }
+
+        public double getGma_album_price() {
+            return gma_album_price;
+        }
+
+
+
+        public void setMt_gma_album_id (String gma_album_id) {
+            this.gma_album_id = gma_album_id;
+        }
+        public void setMt_gma_album_title(String gma_album_title) {
+
+            this.gma_album_title = gma_album_title;
+        }
+
+        public void setMt_gma_album_image_url(String gma_album_image_url) {
+            this.gma_album_image_url = gma_album_image_url;
+        }
+
+        public void setMt_gma_album_artist_name(String gma_album_artist_name) {
+            this.gma_album_artist_name = gma_album_artist_name;
+        }
+
+        public void setGma_album_price (double gma_album_price) {
+            this.gma_album_price = gma_album_price;
+        }
+
+
 }
--- app/src/main/java/com/truthuniversal/tujasiri/truthuniversalapp/GoogleMusicSongItem.java
+++ app/src/main/java/com/truthuniversal/tujasiri/truthuniversalapp/GoogleMusicSongItem.java
@@ -4,4 +4,49 @@ package com.truthuniversal.tujasiri.truthuniversalapp;
  * Created by tujasiri on 6/14/2016.
  */
 public class GoogleMusicSongItem {
+
+    private String gs_song_id = "";
+    private String gs_song_title = "";
+    private double gs_song_price = 0.0;
+
+
+    public GoogleMusicSongItem(){
+        String gs_song_id = "";
+        String gs_song_title = "";
+        double gs_song_price = 0.0;
+
+
+        this.gs_song_id = gs_song_id;
+        this.gs_song_title = gs_song_title;
+        this.gs_song_price = gs_song_price;
+    }
+
+    public String getGs_song_id() {
+        return gs_song_id;
+    }
+
+    public String getGs_song_title() {
+        return gs_song_title;
+    }
+
+    public double getGs_song_price() {
+        return gs_song_price;
+    }
+
+
+
+
+    public void setGs_song_id(String gs_song_id) {
+        this.gs_song_id = gs_song_id;
+    }
+
+    public void setGs_song_title(String gs_song_title) {
+        this.gs_song_title = gs_song_title;
+    }
+
+    public void setGs_song_price(double gs_song_price) {
+        this.gs_song_price = gs_song_price;
+    }
+
+
 }
